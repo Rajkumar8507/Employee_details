5757f4c9be4a354743049e682e30cb1bfa6ceea5
Rajkumar8507 <jamallarajkumar@gmail.com>
2024-11-07 22:32:56 +0530

    Add project files.

---
+++ Employee_details/Default.aspx
@@ -0,0 +1,121 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Employee_details._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+       <div style="padding:15px">
+           <table class="w-100">
+               <tr>
+                   <td style="width: 350px" >EmpId</td>
+                   <td>
+                       <asp:TextBox ID="TextBox1" runat="server" CausesValidation="True" CssClass="css_id" OnTextChanged="TextBox1_TextChanged" ToolTip="enter Id"></asp:TextBox>
+                   </td>
+               </tr>
+               <tr>
+                   <td style="width: 350px" >EmpName</td>
+                   <td>
+                       <asp:TextBox ID="TextBox2" runat="server" CssClass="css_name"></asp:TextBox>
+                   </td>
+               </tr>
+               <tr>
+                   <td style="width: 350px">DoB</td>
+                   <td>
+                       <asp:TextBox ID="TextBox3" runat="server" CssClass="css_dob"></asp:TextBox>
+                   </td>
+               </tr>
+               <tr>
+                   <td style="width: 350px">Gender</td>
+                   <td>
+                       <asp:RadioButtonList ID="RadioButtonList1" runat="server" RepeatDirection="Horizontal" Width="163px">
+                           <asp:ListItem>Male</asp:ListItem>
+                           <asp:ListItem>Female</asp:ListItem>
+                       </asp:RadioButtonList>
+                   </td>
+               </tr>
+               <tr>
+                   <td style="width: 350px">Contact</td>
+                   <td>
+                       <asp:TextBox ID="TextBox4" runat="server" CssClass="css_contact"></asp:TextBox>
+                   </td>
+               </tr>
+               </table>
+           <p>
+           </p>
+           <table class="w-100">
+               <tr>
+    <td style="width: 852px">
+        
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Insert" CssClass="insert" />
+        <asp:Button ID="Button2" runat="server" Text="Update" OnClick="Button2_Click" CssClass="update" />
+        <asp:Button ID="Button3" runat="server"  Text="Delete" OnClick="Button3_Click" CssClass="delete"/>
+    </td>
+    <td style="width: 350px">
+        </td>
+</tr>
+               <tr>
+    <td style="width: 350px" colspan="2">
+    </td>
+</tr>
+<tr>
+    <td colspan="2">
+        <asp:GridView ID="GridView1" runat="server" BackColor="White" BorderColor="#CCCCCC" BorderStyle="None" BorderWidth="1px" CellPadding="3" Width="1081px">
+            <FooterStyle BackColor="White" ForeColor="#000066" />
+            <HeaderStyle BackColor="#006699" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="White" ForeColor="#000066" HorizontalAlign="Left" />
+            <RowStyle ForeColor="#000066" />
+            <SelectedRowStyle BackColor="#669999" Font-Bold="True" ForeColor="White" />
+            <SortedAscendingCellStyle BackColor="#F1F1F1" />
+            <SortedAscendingHeaderStyle BackColor="#007DBB" />
+            <SortedDescendingCellStyle BackColor="#CAC9C9" />
+            <SortedDescendingHeaderStyle BackColor="#00547E" />
+        </asp:GridView>
+    </td>
+</tr>
+
+               </table>
+           <p>
+               </p>
+           
+    </div>
+    <div>
+        <style>
+                .css_id
+                {
+                    border-color:aqua;
+                   
+                }
+                .css_id
+{
+    border-color:aqua;
+   
+}.css_name
+{
+    border-color:aqua;
+   
+}.css_dob
+{
+    border-color:aqua;
+    
+}
+ .css_contact
+{
+    border-color:aqua;
+    
+}
+            .insert
+            {
+                background-color:yellowgreen
+            }
+            .delete
+            {
+                background-color:red;
+            }
+            .update
+            {
+                background-color:gold;
+            }
+        </style>
+        
+    </div>
+   
+
+    </asp:Content>
---
+++ Employee_details/Default.aspx.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Employee_details
+{
+    public partial class _Default : Page
+    {
+        // Connection string defined here
+        private string connectionString = "Data Source=HEMANTH\\SQLEXPRESS2019;Initial Catalog=employee_details;Integrated Security=True";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                getemployeelist();
+            }
+            TextBox1.Focus();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int empId = int.Parse(TextBox1.Text);
+            int contact = int.Parse(TextBox4.Text);
+            string empName = TextBox2.Text;
+            string dob = TextBox3.Text;
+            string gender = RadioButtonList1.SelectedValue;
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    con.Open();
+                    // Use parameterized query to prevent SQL injection
+                    SqlCommand cmd = new SqlCommand("INSERT INTO Employee_details (EmpId, EmpName, DoB,Gender, Contact) VALUES (@EmpId, @EmpName, @DoB,@Gender, @Contact)", con);
+                    cmd.Parameters.AddWithValue("@EmpId", empId);
+                    cmd.Parameters.AddWithValue("@EmpName", empName);
+                    cmd.Parameters.AddWithValue("@DoB", dob);
+                    cmd.Parameters.AddWithValue("@Gender", gender);
+                    cmd.Parameters.AddWithValue("@Contact", contact);
+
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Successfully saved.');", true);
+                    getemployeelist();
+                }
+                catch (Exception ex)
+                {
+                    // Handle exception
+                    ScriptManager.RegisterStartupScript(this, this.GetType(), "script", $"alert('Error: {ex.Message}');", true);
+                }
+            }
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+
+            int empId = int.Parse(TextBox1.Text);
+            int contact = int.Parse(TextBox4.Text);
+            string empName = TextBox2.Text;
+            string dob = TextBox3.Text;
+            string gender = RadioButtonList1.SelectedValue;
+
+            // Update logic can be implemented here
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+
+                try
+                {
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand("UPDATE Employee_details SET EmpName=@EmpName,DoB=@DoB,Gender=@Gender,Contact=@Contact WHERE EmpId=@EmpId", con);
+                    cmd.Parameters.AddWithValue("@EmpId", empId);
+                    cmd.Parameters.AddWithValue("@EmpName", empName);
+                    cmd.Parameters.AddWithValue("@DoB", dob);
+                    cmd.Parameters.AddWithValue("@Gender", gender);
+                    cmd.Parameters.AddWithValue("@Contact", contact);
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('successfully updated');", true);
+                    getemployeelist();
+                }
+
+                catch (Exception ex1)
+                {
+                    ScriptManager.RegisterStartupScript(this, this.GetType(), "script", $"alert('Error:{ex1.Message}');", true);
+                }
+
+            }
+        }
+
+
+        void getemployeelist()
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand("SELECT * FROM Employee_details", con);
+                    SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    sda.Fill(dt);
+                    GridView1.DataSource = dt;
+                    GridView1.DataBind();
+                }
+                catch (Exception ex)
+                {
+                    // Handle exception
+                    ScriptManager.RegisterStartupScript(this, this.GetType(), "script", $"alert('Error retrieving data: {ex.Message}');", true);
+                }
+            }
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            int empId = int.Parse(TextBox1.Text);
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+
+                try
+                {
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand("delete Employee_details   WHERE EmpId=@EmpId", con);
+                    cmd.Parameters.AddWithValue("@EmpId", empId);
+                    
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Successfully deleted.');", true);
+                    getemployeelist();
+                }
+                catch (Exception ex3)
+                {
+                    // Handle exception
+                    ScriptManager.RegisterStartupScript(this, this.GetType(), "script", $"alert('Error: {ex3.Message}');", true);
+                }
+            }
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
